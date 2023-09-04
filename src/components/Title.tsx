02d32a2ccc67951f5a5be2a3cbdaecb6a99f1ac6
maayanalbert <maayan.albert@gmail.com>
2023-09-04 13:50:52 -0400

    Update Title.tsx

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,4 +1,4 @@
-import { useCardSize, useFadeInTitle, useIsMobile } from "@/GlobalsContext"
+import { useCardSize, useIsMobile } from "@/GlobalsContext"
 import getBrowserType from "@/helpers/getBrowserType"
 import getOnIpad from "@/helpers/getOnIpad"
 import { useEffect, useRef, useState } from "react"
@@ -9,9 +9,6 @@ import { useEffect, useRef, useState } from "react"
 export default function Title() {
   const isMobile = useIsMobile()
   const cardSize = useCardSize()
-  const fadeInTitle = useFadeInTitle()
-  const [spaceNameEdited, setSpaceNameEdited] = useState(false)
-  const spaceNameRef = useRef<HTMLDivElement>(null)
 
   return (
     <div
@@ -19,28 +16,14 @@ export default function Title() {
     text-center text-white flex flew-row justify-center"
       style={{
         paddingBottom: isMobile ? cardSize / 3 : 0,
-        gap: isMobile ? (spaceNameEdited ? 7 : 15) : spaceNameEdited ? 9 : 18,
       }}
     >
       <div
-        className="scroll-your relative"
-        style={{ paddingLeft: isMobile ? 10 : 16 }}
-      >
-        <i
-          ref={spaceNameRef}
-          className={`${isMobile ? "font-extralight" : "font-thin "} ${
-            fadeInTitle ? "animate-your" : ""
-          } whitespace-nowrap cursor-default select-none`}
-        >
-          Your
-        </i>
-      </div>
-      <div
         className={`scroll-space ${
-          isMobile ? "font-bold" : "font-medium"
+          isMobile ? "font-normal" : "font-extralight"
         } select-none cursor-default`}
       >
-        <p className={fadeInTitle ? "animate-space" : ""}>Space</p>
+        <p style={{ color: "gray" }}>What's on your mind?</p>
       </div>
     </div>
   )
