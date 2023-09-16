b62aa4d51d9c0e998a321ea5574233b6ae527a30
maayanalbert <maayan.albert@gmail.com>
2023-09-16 15:07:44 -0700

    Remove spaces buttons

--- src/components/MainPage.tsx
+++ src/components/MainPage.tsx
@@ -1,7 +1,7 @@
 import Title from "@/components/Title"
 import InfoCard from "@/components/InfoCard"
 import AccessCard from "@/components/AccessCard"
-import SpaceButtons from "./SpaceButtons"
+
 /**
  * The main page where all of the content is
  */
