d926a43f26fe43c020dc654107692eb3b716f0bd
maayanalbert <maayan.albert@gmail.com>
2023-10-27 14:01:13 -0700

    Revert "tried moving"
    This reverts commit c3de583f0b00ba04d5ac620d5ff6eb48805fa068.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,6 @@
 import { use, useEffect, useState } from "react"
 import { useRouter } from "next/router"
 import { useMousePosition } from "@/utils/useMousePosition"
-import { useMouseMove } from "@/utils/useMouseMove"
 
 /**
  * A wrapper for the main page
@@ -9,69 +8,18 @@ import { useMouseMove } from "@/utils/useMouseMove"
 export default function Home() {
   useEffect(() => {
     document.title = "Eve"
-    const centerX = window.innerWidth / 2
-    const centerY = window.innerHeight / 2
-    // Set the initial position of the dot to the center
-    setDotX(centerX)
-    setDotY(centerY)
   }, [])
 
-  // const [mouseX, setMouseX] = useState(0)
-  // const [mouseY, setMouseY] = useState(0)
+  const { mouseX, mouseY } = useMousePosition()
   const [isInside, setIsInside] = useState(false)
-  const radius = 66 / 2
+
+  const radius = 77
   const [dotX, setDotX] = useState(0)
   const [dotY, setDotY] = useState(0)
 
-  useMouseMove(
-    (event: MouseEvent) => {
-      const newIsInside = getIsInside(
-        dotX,
-        dotY,
-        radius,
-        event.clientX,
-        event.clientY
-      )
-      if (isInside && !newIsInside) {
-        const angle = Math.atan2(event.clientY - dotY, event.clientX - dotX)
-        const newDotX = dotX + Math.cos(angle) * event.movementX
-        const newDotY = dotY + Math.sin(angle) * event.movementY
-        setDotX(newDotX)
-        setDotY(newDotY)
-        const newNewIsInside = getIsInside(
-          newDotX,
-          newDotX,
-          radius,
-          event.clientX,
-          event.clientY
-        )
-        setIsInside(newNewIsInside)
-      } else {
-        setIsInside(newIsInside)
-      }
-    },
-    [isInside]
-  )
-
   return (
-    <div className="h-full w-full">
-      <div
-        className="rounded-full bg-white cursor-pointer h-[66px] w-[66px] absolute"
-        style={{ top: dotY - radius, left: dotX - radius }}
-      />
+    <div className="h-full w-full flex justify-center items-center">
+      <div className="rounded-full bg-white cursor-pointer h-[66px] w-[66px] hover:h-[77px] hover:w-[77px] transition-all duration-300 ease-in-out" />
     </div>
   )
-}
-
-// hover:h-[77px] hover:w-[77px] transition-all duration-300 ease-in-out
-
-function getIsInside(
-  x: number,
-  y: number,
-  radius: number,
-  mouseX: number,
-  mouseY: number
-) {
-  const distance = Math.sqrt((x - mouseX) ** 2 + (y - mouseY) ** 2)
-  return distance < radius
 }
