ff19e50919964915ec6ae78fa2cd16fd8b69cdd6
maayanalbert <maayan.albert@gmail.com>
2023-10-29 16:59:55 -0700

    tried shadow

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,12 +8,14 @@ import { useEffect, useState } from "react"
 export default function Home() {
   useEffect(() => {
     document.title = "Eve"
+    setInnerWidth(window.innerWidth)
   }, [])
 
   const [state, setState] = useState<"CLOSED" | "OPEN" | "FINISHED">("CLOSED")
   const [finishable, setFinishable] = useState(false)
   const [showCursor, setShowCursor] = useState(true)
   const [finishedVisible, setFinishedVisible] = useState(false)
+  const [innerWidth, setInnerWidth] = useState(0)
 
   useMouseMove(() => {
     if (state === "OPEN" && finishable) {
@@ -23,7 +25,7 @@ export default function Home() {
 
   return (
     <div className={`${showCursor ? "" : "cursor-none"}  h-full w-full`}>
-      {state === "FINISHED" && (
+      {state === "FINISHED" && ( // learn more text
         <div
           className={`h-full w-full flex justify-center items-center absolute`}
         >
@@ -45,20 +47,25 @@ export default function Home() {
         }`}
       >
         <div
-          className={`rounded-full bg-white flex justify-center items-center ${
-            showCursor ? "cursor-pointer" : ""
+          className={`flex justify-center items-center fade-in-dot rounded-full ${
+            showCursor ? "cursor-pointer" : "" // dot
           } ${
             state === "CLOSED"
-              ? "h-[66px] w-[66px] hover:h-[77px] hover:w-[77px] duration-300"
+              ? "h-[66px] bg-white w-[66px] hover:h-[77px] hover:w-[77px] duration-300"
               : state === "OPEN"
-              ? `
-              sm:h-[500px] sm:w-[500px] 
-              h-[400px] w-[400px] ${
-                showCursor ? "duration-300" : "duration-700"
-              }`
-              : "h-0 w-0 duration-700"
+              ? `shadow-[inset_0_0_10px_gray]
+              ${showCursor ? "duration-300" : "duration-[2000ms]"}`
+              : `h-0 w-0 duration-[2000ms]`
           }
          transition-all ease-in-out`}
+          style={{
+            background:
+              state === "OPEN" || state === "FINISHED"
+                ? "rgb(240, 240, 240"
+                : "white",
+            width: state === "OPEN" ? innerWidth * 2 : undefined,
+            height: state === "OPEN" ? innerWidth * 2 : undefined,
+          }}
           onClick={() => {
             if (state === "CLOSED") {
               setState("OPEN")
@@ -71,8 +78,12 @@ export default function Home() {
           }}
         >
           <div
-            className="whitespace-nowrap transition-all ease-in-out duration-1000 flex flex-row gap-1 text-xl select-none"
-            style={{ opacity: state === "OPEN" ? 1 : 0 }}
+            className={`whitespace-nowrap transition-all ease-in-out 
+            duration-1000 flex flex-row gap-1 text-xl select-none`}
+            style={{
+              opacity: state === "OPEN" ? 1 : 0,
+              color: "rgb(44, 44, 44)",
+            }}
           >
             Eve is a space to talk to yourself
           </div>
