c6bc0fd95fde52240fc43adc1228742b30b69e67
maayanalbert <maayan.albert@gmail.com>
2023-11-04 12:55:41 -0400

    tried scroll down

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,10 @@
+import { getMappedValue } from "@/utils/getMappedValue"
 import useEventListener from "@/utils/useEventListener"
 import { useMousePercentToCenterCss } from "@/utils/useMousePercentToCenterCss"
-import { useEffect, useState } from "react"
+import { get, min } from "lodash"
+import { useEffect, useRef, useState } from "react"
+
+const startSize = 77
 
 /**
  * A wrapper for the main page
@@ -10,114 +14,76 @@ export default function Home() {
     document.title = "Eve"
   }, [])
 
-  const [state, setState] = useState<"CLOSED" | "OPEN" | "FINISHED">("CLOSED")
-  const [finishable, setFinishable] = useState(false)
-  const [showCursor, setShowCursor] = useState(true)
-  const [finishedVisible, setFinishedVisible] = useState(false)
+  const scrollPercent = useRef(0)
 
-  useMousePercentToCenterCss()
+  useEffect(() => {
+    document.documentElement.style.setProperty("--womb-size", `${startSize}px`)
+    document.documentElement.style.setProperty("--overflow-status", "hidden")
+  })
 
   useEventListener(
-    "mousemove",
-    () => {
-      if (state === "OPEN" && finishable) {
-        setShowCursor(true)
-      }
+    "wheel",
+    (event: WheelEvent) => {
+      const maxSize = Math.min(
+        window.innerWidth * 0.77,
+        window.innerHeight * 0.77
+      )
+
+      const minSize = startSize
+      const dist = maxSize - minSize
+
+      const deltaPercent = event.deltaY / dist
+
+      ;(scrollPercent.current = Math.max(
+        0,
+        scrollPercent.current + deltaPercent
+      )),
+        1
+
+      const easeKeyframe = getMappedValue(
+        scrollPercent.current,
+        0,
+        1,
+        0,
+        1,
+        easeInQuad
+      )
+
+      const size = minSize + dist * easeKeyframe
+
+      document.documentElement.style.setProperty("--womb-size", `${size}px`)
+      document.documentElement.style.setProperty(
+        "--womb-percent",
+        `${scrollPercent.current}`
+      )
+      document.documentElement.style.setProperty(
+        "--overflow-status",
+        scrollPercent.current === 1 ? "auto" : "hidden"
+      )
     },
-    [state, finishable]
+    []
   )
 
   return (
-    <div className={`${!showCursor ? "cursor-none" : ""}  h-full w-full`}>
-      {state === "FINISHED" && ( // learn more text
-        <div
-          className={`h-full w-full flex flex-col justify-center items-center absolute sm:gap-1.5 gap-1`}
-        >
-          <div
-            className="transition-all duration-500 sm:text-xl text-lg"
-            style={{ color: "gray", opacity: finishedVisible ? 1 : 0 }}
-          >
-            <p>What's inside yours?</p>
-          </div>
-          <div
-            className="transition-all duration-500 flex flex-row gap-1.5 sm:text-base text-sm"
-            style={{ color: "gray", opacity: finishedVisible ? 1 : 0 }}
-          >
-            <p>Contact</p>
-            <a className="underline" href={"mailto:maaayan@eve.space"}>
-              maayan@eve.space
-            </a>
-            <p>to learn more</p>
-          </div>
-        </div>
-      )}
+    <div style={{ width: "100%", height: "200%" }}>
       <div
-        className={`h-full w-full flex justify-center items-center ${
-          state === "FINISHED" ? "pointer-events-none" : ""
-        }`}
+        className="w-full flex justify-center items-center"
+        style={{ height: "50%" }}
       >
-        <div
-          className={`flex justify-center items-center rounded-full ${
-            showCursor ? "cursor-pointer" : "" // womb
-          } ${
-            state === "CLOSED"
-              ? "h-[132px] w-[132px] hover:h-[150px] hover:w-[150px] duration-300"
-              : state === "OPEN"
-              ? `sm:h-[550px] sm:w-[550px] w-[400px] h-[400px]
-              ${showCursor ? "duration-300" : "duration-1000"}`
-              : `h-0 w-0 duration-1000`
-          }
-         transition-all ease-in-out`}
-          onClick={() => {
-            if (state === "CLOSED") {
-              setState("OPEN")
-              setShowCursor(false)
-              setTimeout(() => setFinishable(true), 700)
-            } else if (state === "OPEN") {
-              setState("FINISHED")
-              setTimeout(() => setFinishedVisible(true), 950)
-            }
-          }}
-        >
-          <div
-            className={`fade-in-womb rounded-full h-full w-full bg-white
-            flex justify-center items-center overflow-hidden ${
-              state === "OPEN"
-                ? "shadow-[inset_0_0_10px_rgb(50_50_50)]"
-                : state === "FINISHED"
-                ? "shadow-[inset_0_0_5px_gray]"
-                : `expand-womb`
-            }`}
-          >
-            <div
-              className={`whitespace-nowrap flex flex-col sm:text-4xl text-2xl select-none h-full w-full justify-center items-center rounded-full
-              transition-all ease-in-out duration-500 ${
-                state === "CLOSED" ? "opacity-0 hover:opacity-[.15]" : ""
-              }`}
-              style={{
-                color: "rgb(44, 44, 44)",
-                filter: state === "CLOSED" ? "blur(2px)" : undefined,
-              }}
-            >
-              Within each soul lies a universe
-              <div className="relative w-full">
-                <div
-                  className="absolute w-full text-center text-base top-4"
-                  style={{ color: "gray" }}
-                >
-                  <a
-                    className="underline"
-                    href="https://www.sefaria.org/Mishnah_Sanhedrin.3.8?lang=bi"
-                    target="_blank"
-                  >
-                    Rabbinic Precept
-                  </a>
-                </div>
-              </div>
-            </div>
-          </div>
+        <div className="rounded-full expand-womb bg-white fade-in-womb flex h-full w-full justify-center items-center">
+          {/* <p className="whitespace-nowrap select-none">Welcome to Eve</p> */}
         </div>
+      </div>
+      <div style={{ width: "100%", height: "50%" }}>
+        Rabbinic texts something something
       </div>
     </div>
   )
 }
+
+function easeInQuad(x: number): number {
+  return x * x
+}
+
+// Ancient rabbinic texts state that, as Adam was one person, from whom the population of an entire world came forth
+// Ancient rabbinic texts state that
--- src/utils/useMousePercentToCenterCss.tsx
+++ src/utils/useMousePercentToCenterCss.tsx
@@ -6,28 +6,12 @@ import { getMappedValue } from "./getMappedValue"
  * Called on mouse move
  */
 export function useMousePercentToCenterCss() {
-  const callback = (event: MouseEvent) => {
-    const dist = getDist(
-      event.clientX,
-      event.clientY,
-      window.innerWidth / 2,
-      window.innerHeight / 2
-    )
+  const callback = (event: WheelEvent) => {
+    const height = window.innerHeight
 
-    const maxDist = getDist(0, 0, window.innerWidth / 2, window.innerHeight / 2)
-    const adjustedDist = dist <= 130 / 2 ? 0 : dist - 130 / 2 // perhaps make a bit smaller than actual size
-    const percentToCenter = 1 - adjustedDist / maxDist
-    const val = percentToCenter
-    document.documentElement.style.setProperty("--mouse-dist", `${val}`)
-
-    const mouseScale = getMappedValue(val, 0, 1, 0.5, 1)
-
-    document.documentElement.style.setProperty("--mouse-scale", `${mouseScale}`)
+    console.log(event.deltaY)
+    // document.documentElement.style.setProperty("--mouse-scale", `${mouseScale}`)
   }
 
-  useEventListener("mousemove", callback)
-}
-
-function getDist(x1: number, y1: number, x2: number, y2: number) {
-  return Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2))
+  useEventListener("wheel", callback)
 }
