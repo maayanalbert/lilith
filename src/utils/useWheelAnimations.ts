7273264b9a32a96ce5aaf4d27eec1c85ebe1ab7e
maayanalbert <maayan.albert@gmail.com>
2023-11-06 18:45:33 -0500

    Update useWheelAnimations.ts

--- src/utils/useWheelAnimations.ts
+++ src/utils/useWheelAnimations.ts
@@ -48,96 +48,75 @@ export function useWheelAnimations(
         maxSize
       )
 
-      document.documentElement.style.setProperty(
-        "--womb-size",
-        `${wombSize.current}px`
-      )
-
-      const wombBlur = getMappedValue(
+      updateWheelAnimations(
         wombSize.current,
-        startSize,
         maxSize,
-        0,
-        10
+        setIsInsideWomb,
+        scrollable
       )
-      document.documentElement.style.setProperty("--womb-blur", `${wombBlur}px`)
-
-      const hintScale = getMappedValue(
-        wombSize.current,
-        startSize,
-        maxSize,
-        1,
-        24
-      )
-      document.documentElement.style.setProperty("--hint-scale", `${hintScale}`)
-
-      document.documentElement.style.setProperty(
-        "--hint-dist",
-        `${wombSize.current * 2}px`
-      )
-
-      const textScale = getMappedValue(
-        wombSize.current,
-        startSize,
-        maxSize,
-        1,
-        12,
-        easeInQuad
-      )
-      document.documentElement.style.setProperty("--text-scale", `${textScale}`)
-
-      const textOpacity =
-        wombSize.current < (2 * maxSize) / 7
-          ? getMappedValue(
-              wombSize.current,
-              startSize,
-              (2 * maxSize) / 7,
-              0,
-              1,
-              easeInSine
-            )
-          : wombSize.current > (3 * maxSize) / 7
-          ? getMappedValue(wombSize.current, (3 * maxSize) / 7, maxSize, 1, 0)
-          : 1
-
-      document.documentElement.style.setProperty(
-        "--text-opacity",
-        `${textOpacity}`
-      )
-
-      const textBlur =
-        wombSize.current < (2 * maxSize) / 7
-          ? getMappedValue(
-              wombSize.current,
-              startSize,
-              (2 * maxSize) / 7,
-              2,
-              0,
-              easeInSine
-            )
-          : wombSize.current > (3 * maxSize) / 7
-          ? getMappedValue(wombSize.current, (3 * maxSize) / 7, maxSize, 0, 2)
-          : 0
-
-      document.documentElement.style.setProperty("--text-blur", `${textBlur}px`)
-
-      const blurbOpacity = wombSize.current === maxSize ? 1 : 0
-      document.documentElement.style.setProperty(
-        "--blurb-opacity",
-        `${blurbOpacity}`
-      )
-
-      setIsInsideWomb(wombSize.current === maxSize)
-
-      if (wombSize.current === maxSize && !scrollable.current) {
-        scrollable.current = true
-        setTimeout(() => {
-          document.documentElement.style.setProperty("--html-overflow", `auto`)
-        }, 750)
-      }
     },
     []
   )
+}
+
+function updateWheelAnimations(
+  wombSize: number,
+  maxSize: number,
+  setIsInsideWomb: (isInsideWomb: boolean) => void,
+  scrollable: MutableRefObject<boolean>
+) {
+  document.documentElement.style.setProperty("--womb-size", `${wombSize}px`)
+
+  const wombBlur = getMappedValue(wombSize, startSize, maxSize, 0, 10)
+  document.documentElement.style.setProperty("--womb-blur", `${wombBlur}px`)
+
+  const hintScale = getMappedValue(wombSize, startSize, maxSize, 1, 24)
+  document.documentElement.style.setProperty("--hint-scale", `${hintScale}`)
+
+  document.documentElement.style.setProperty("--hint-dist", `${wombSize * 2}px`)
+
+  const textScale = getMappedValue(
+    wombSize,
+    startSize,
+    maxSize,
+    1,
+    12,
+    easeInQuad
+  )
+  document.documentElement.style.setProperty("--text-scale", `${textScale}`)
+
+  const textOpacity =
+    wombSize < (2 * maxSize) / 7
+      ? getMappedValue(wombSize, startSize, (2 * maxSize) / 7, 0, 1, easeInSine)
+      : wombSize > (3 * maxSize) / 7
+      ? getMappedValue(wombSize, (3 * maxSize) / 7, maxSize, 1, 0)
+      : 1
+
+  document.documentElement.style.setProperty("--text-opacity", `${textOpacity}`)
+
+  const textBlur =
+    wombSize < (2 * maxSize) / 7
+      ? getMappedValue(wombSize, startSize, (2 * maxSize) / 7, 2, 0, easeInSine)
+      : wombSize > (3 * maxSize) / 7
+      ? getMappedValue(wombSize, (3 * maxSize) / 7, maxSize, 0, 2)
+      : 0
+
+  document.documentElement.style.setProperty("--text-blur", `${textBlur}px`)
+
+  const blurbOpacity = wombSize === maxSize ? 1 : 0
+  document.documentElement.style.setProperty(
+    "--blurb-opacity",
+    `${blurbOpacity}`
+  )
+
+  setIsInsideWomb(wombSize === maxSize)
+
+  if (wombSize === maxSize && !scrollable.current) {
+    scrollable.current = true
+    setTimeout(() => {
+      document.documentElement.style.setProperty("--html-overflow", `auto`)
+    }, 750)
+  }
 }
 
 function getDist(x1: number, y1: number, x2: number, y2: number) {
