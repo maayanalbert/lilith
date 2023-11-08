f20aef564ab3580aa455cf295fc73f044242635a
maayanalbert <maayan.albert@gmail.com>
2023-11-08 16:00:54 -0500

    turned animations off on mobile

--- src/components/ExpandingTitle.tsx
+++ src/components/ExpandingTitle.tsx
@@ -1,4 +1,11 @@
+import { useEffect, useState } from "react"
+
 export default function ExpandingTitle() {
+  const [windowWidth, setWindowWidth] = useState(640)
+
+  useEffect(() => {
+    setWindowWidth(window.innerWidth)
+  }, [])
   return (
     <div
       className="flex justify-center items-center bg-black h-full"
@@ -7,7 +14,11 @@ export default function ExpandingTitle() {
         marginLeft: "-100%",
       }}
     >
-      <div className="rounded-full womb bg-white womb-fade-in" />
+      <div
+        className={`rounded-full womb bg-white ${
+          windowWidth >= 640 && "womb-fade-in"
+        }`}
+      />
       <div className="absolute flex h-full w-full justify-center items-center top-0">
         <p className="whitespace-nowrap select-none title sm:text-4xl text-xl cursor-default">
           Welcome to Eve
@@ -15,7 +26,9 @@ export default function ExpandingTitle() {
       </div>
       <div className="absolute">
         <p
-          className="hint-fade-in expand-hint font-light text-3xl"
+          className={`${
+            windowWidth >= 640 && "hint-fade-in"
+          } expand-hint font-light text-3xl`}
           style={{ color: "gray" }}
         >
           {"(scroll)"}
