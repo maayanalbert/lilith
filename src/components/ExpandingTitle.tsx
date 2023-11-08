b4328f4fe4c862d61e1e546b59b0b3ac429c857b
maayanalbert <maayan.albert@gmail.com>
2023-11-08 15:11:35 -0500

    try to save animation

--- src/components/ExpandingTitle.tsx
+++ src/components/ExpandingTitle.tsx
@@ -1,4 +1,8 @@
-export default function ExpandingTitle() {
+interface Props {
+  isRendered: boolean
+}
+
+export default function ExpandingTitle({ isRendered }: Props) {
   return (
     <div
       className="flex justify-center items-center bg-black h-full"
@@ -7,7 +11,11 @@ export default function ExpandingTitle() {
         marginLeft: "-100%",
       }}
     >
-      <div className="rounded-full womb bg-white womb-fade-in" />
+      <div
+        className={`rounded-full womb bg-white ${
+          isRendered ? "womb-fade-in" : ""
+        }`}
+      />
       <div className="absolute flex h-full w-full justify-center items-center top-0">
         <p className="whitespace-nowrap select-none title sm:text-4xl text-xl cursor-default">
           Welcome to Eve
@@ -15,7 +23,9 @@ export default function ExpandingTitle() {
       </div>
       <div className="absolute">
         <p
-          className="hint-fade-in expand-hint font-light text-3xl"
+          className={`${
+            isRendered ? "hint-fade-in" : ""
+          } expand-hint font-light text-3xl`}
           style={{ color: "gray" }}
         >
           {"(scroll)"}
