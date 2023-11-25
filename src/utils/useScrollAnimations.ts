53436300c0c0b2894339a29075ffb08ef884adb3
maayanalbert <maayan.albert@gmail.com>
2023-11-25 09:51:07 -0500

    Update useScrollAnimations.ts

--- src/utils/useScrollAnimations.ts
+++ src/utils/useScrollAnimations.ts
@@ -193,7 +193,3 @@ function updateTitleStyle(wombSize: number, maxSize: number) {
 
   title.style.opacity = `${titleOpacity}`
 }
-
-export function getDist(x1: number, y1: number, x2: number, y2: number) {
-  return Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2))
-}
