6dcbe309427bfd05d7ee0d287eed49bf11f5d21b
maayanalbert <maayan.albert@gmail.com>
2023-12-07 20:43:19 -0500

    Revert "Update useScrollAnimations.ts"
    This reverts commit d85ec4f21aec98efafbf3edc83e34b3f6afba6d9.

--- src/components/Womb.tsx
+++ src/components/Womb.tsx
@@ -1,17 +1,23 @@
 import useEventListener from "@/utils/useEventListener"
 import { startSize } from "@/utils/useScrollAnimations"
 import { set } from "lodash"
-import { useEffect, useState } from "react"
+import { useState } from "react"
 
 export default function Womb() {
   const [scrolled, setScrolled] = useState(false)
-  const [isMobile, setIsMobile] = useState(false)
+  const [scrolledToBottom, setScrolledToBottom] = useState(false)
 
-  useEventListener("scroll", () => setScrolled(true), [])
-
-  const textMarginTop = startSize * 2.05
-
-  useEffect(() => setIsMobile(window.innerWidth < 640))
+  useEventListener(
+    "scroll",
+    () => {
+      setScrolled(true)
+      const maxScroll = document.body.scrollHeight - window.innerHeight
+      if (window.scrollY >= maxScroll) {
+        setScrolledToBottom(true)
+      }
+    },
+    []
+  )
 
   return (
     <div className="w-full relative" style={{ height: "100svh" }}>
@@ -26,48 +32,32 @@ export default function Womb() {
         <div
           className={`${
             !scrolled && "hint-enter"
-          } font-light w-full flex flex-row justify-center sm:text-[28px] text-[30px] text-zinc-600`}
+          } font-light w-full flex flex-row justify-center ${
+            scrolledToBottom ? "text-[24px]" : "text-[24px]"
+          } text-zinc-500`}
           style={{
-            marginTop: textMarginTop,
-            opacity: 0.85,
+            marginTop: scrolledToBottom ? startSize * 1.8 : startSize * 1.85,
           }}
         >
-          <p className="hint">{"(scroll)"}</p>
+          <p className="hint">
+            {scrolledToBottom ? "Take a Bite" : "(scroll)"}
+          </p>
         </div>
       </div>
-      <div
-        className={`h-full w-full ${!scrolled ? "womb-enter" : ""}`} // womb
-      >
-        <div
-          className={`womb absolute rounded-full bg-white`}
-          style={{
-            transform: "translate(-50%, -50%)",
-            top: "50%",
-            left: "50%",
-            transformOrigin: "50% 50%",
-            zIndex: -1,
-            height: startSize,
-            width: startSize,
-          }}
-        />
-      </div>
-      <div // title
-        className="absolute title w-full"
+      <div // womb
+        className={`womb absolute rounded-full ${
+          !scrolled && "womb-enter"
+        }  bg-white`}
         style={{
           transform: "translate(-50%, -50%)",
           top: "50%",
           left: "50%",
-          opacity: 0,
-          marginTop: textMarginTop * 0.6,
-          fontSize: isMobile ? "38px" : "62px",
+          transformOrigin: "50% 50%",
+          zIndex: -1,
+          height: startSize,
+          width: startSize,
         }}
-      >
-        <p
-          className={`font-display font-bold text-center text-white tracking-[.035em]`}
-        >
-          Welcome to Eve
-        </p>
-      </div>
+      />
     </div>
   )
 }
