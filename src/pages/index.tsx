93ffb9ca02f25a2f5ad6b2bdcacfb922541eade1
maayanalbert <maayan.albert@gmail.com>
2024-02-20 12:43:12 -0500

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,24 +14,27 @@ export default function Home() {
         style={{ height: "100svh" }}
       >
         <div className="flex flex-col justify-center items-center -mt-[9svh] sm:mt-[2svh] blurb">
-          <div className="flex flex-col">
+          <div className="flex flex-col text-center justify-center items-center">
             <a className="font-bold sm:text-[36px] text-[35px] text-white mb-6">
               Welcome to Eve
             </a>
-            <p
-              className="w-[288px] text-white font-light sm:text-[15.5px] text-[15px]"
-              style={{ color: "rgb(235, 235, 240)" }}
-            >
-              A space for your thoughts. To learn more, contact{" "}
-              <a
-                target="_blank"
-                className="underline cursor-pointer"
-                href="mailto:maayan@eve.space"
+            <a className="sm:w-full w-[300px]">
+              <p
+                className="text-white font-light sm:text-[15.5px] text-[15px]"
+                style={{ color: "rgb(235, 235, 240)" }}
               >
-                maayan@eve.space
-              </a>
-              .
-            </p>
+                Eve is an AI diary for young adults to help them navigate their
+                20s. <br /> <br className="sm:hidden" /> To learn more, contact{" "}
+                <a
+                  target="_blank"
+                  className="underline cursor-pointer"
+                  href="mailto:maayan@eve.space"
+                >
+                  maayan@eve.space
+                </a>
+                .
+              </p>
+            </a>
           </div>
           <div className="mt-[44px] mb-[34px]">
             <EmailField />
